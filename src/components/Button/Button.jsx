a5605f037d113d9f3e5755acb986c2662326e90d
vlasiszacharis <vlasisz983@gmail.com>
2023-10-18 14:17:41 +0300

    Button Component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Button.css'
+import {useState} from 'react'
 
-function Button() {
-  return (
-    <div>Button</div>
+function Button({button,currentColor}) {
+  const [color,setColor] = useState(currentColor)
+  const [initial,setInitial]   = useState(currentColor)
+
+  const handleMouse = () => {
+        setColor("dark_grey")
+    }
+
+   const handleMouseOut = ()=>{
+       if(initial==="dark"){
+        setColor("dark")}
+        else{
+            setColor("grey")
+        }
+   } 
+ 
+    return (
+    <>
+        <div className="button_component">
+        <button className={color} type="submit" onMouseOver={handleMouse} onMouseOut={handleMouseOut}>{button}</button>
+
+        </div>
+        
+    </>
   )
 }
 
